8646f74dea57de0274548f27afd741e613b9ac62
Igor812 <92649492+Igor812@users.noreply.github.com>
2023-04-04 22:33:38 +0300

    hw04-gc

--- my-otus-hw/hw04-gc/src/main/java/Summator.java
+++ my-otus-hw/hw04-gc/src/main/java/Summator.java
@@ -2,20 +2,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Summator {
-    private Integer sum = 0;
-    private Integer prevValue = 0;
-    private Integer prevPrevValue = 0;
-    private Integer sumLastThreeValues = 0;
-    private Integer someValue = 0;
+    private int sum = 0;
+    private int prevValue = 0;
+    private int prevPrevValue = 0;
+    private int sumLastThreeValues = 0;
+    private int someValue = 0;
+
     private final List<Data> listValues = new ArrayList<>();
-    private Integer cunter = 0;
-    private Integer size = 0;
+    private int cunter = 0;
+    private int size = 0;
 
     //!!! сигнатуру метода менять нельзя
     public void calc(Data data) {
-       cunter = data.getValue();
-       size = listValues.size();
-       listValues.add(data);
+        cunter = data.getValue();
+        size = listValues.size();
+        listValues.add(data);
         if (size % 6_600_000 == 0) {
             listValues.clear();
         }
