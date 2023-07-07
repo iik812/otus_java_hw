bff08903cac3660747c7682d84ccf0af45904264
Igor812 <92649492+Igor812@users.noreply.github.com>
2023-07-07 21:31:00 +0300

    change project structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,3 @@
-rootProject.name = "my-otus-hw"
-include ("hw01-gradle")
-include("hw04-gc:demo")
-
 pluginManagement {
     val jgitver: String by settings
     val dependencyManagement: String by settings
