6efeceeafa64748aa7b5588c6e437684df7563d1
Igor812 <92649492+Igor812@users.noreply.github.com>
2023-07-11 23:43:31 +0300

    hw11-cache

---
+++ hw11-cache/homework/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("java")
+}
+
+dependencies {
+    implementation("ch.qos.logback:logback-classic")
+    implementation("com.zaxxer:HikariCP")
+    implementation("ch.qos.logback:logback-classic")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.postgresql:postgresql")
+}
